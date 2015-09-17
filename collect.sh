54c79133cb1d740e2c1515f865e97d4b388c7c85
ci <Tristan.T.Jiang@newegg.com>
2015-09-17 16:02:22 +0800

    fix

---
+++ collect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ID=$(cat /proc/self/cgroup | grep -o  -e "docker-.*.scope" | head -n 1 | sed "s/docker-\(.*\).scope/\\1/")
+curl -H "Content-Type: application/json" -X POST -d '{"containerId":"'${ID}'","appId":"'$1'"}' http://10.16.82.141:8080/register/api/container/collection
+
